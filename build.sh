3c141fc2abc45d7273b071d62af3f5f8ee166509
rainboy <rainboylvx@qq.com>
2024-01-02 17:19:36 +0800

    upd

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-./node_modules/.bin/parcel build --no-cache --no-content-hash
+# ./node_modules/.bin/parcel build --no-cache --no-content-hash
+yarn build
 
 node ./bin/render_markdown.js
 
 cp -r ./src/prism-theme ./dist/
 
 ./node_modules/.bin/sass ./src/markdown-style/markdown.scss ./dist/markdown.css
+
+make -C ./images/
